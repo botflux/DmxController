fa71ab3f322edbb81754cb7ed06d411d659ec8dc
botf <victor.mendele68@gmail.com>
2018-03-27 16:22:57 +0200

    Ajout viewmodel et view dans des fichiers séparé avec des exemples de binding

--- MaterialDesignTest/ViewModels/ApplicationViewModel.cs
+++ MaterialDesignTest/ViewModels/ApplicationViewModel.cs
@@ -1,17 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using VPackage.Network;
 
-namespace DmxController
+namespace DmxController.ViewModels
 {
-    public class ApplicationViewModel
+    public class ApplicationViewModel : ViewModel, IPageViewModel
     {
-        public ApplicationViewModel ()
+
+        #region Fields
+
+        private ICommand changePageCommand;
+        private IPageViewModel currentPageViewModel;
+        private List<IPageViewModel> pageViewModels;
+
+        #endregion
+
+        #region Propreties / Commands
+
+        public ICommand ChangePageCommand
         {
-            UtilityProvider.Current.ProvideNetworkManager(new NetworkManager("10.129.22.26", 5000, 15000, 15000));
+            get
+            {
+                return changePageCommand;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "Application";
+            }
+        }
+
+        public IPageViewModel CurrentPageViewModel
+        {
+            get
+            {
+                return currentPageViewModel;
+            }
+
+            set
+            {
+                if (currentPageViewModel != value)
+                {
+                    currentPageViewModel = value;
+                    NotifyProperty();
+                }
+            }
+        }
+
+        public List<IPageViewModel> PageViewModels
+        {
+            get
+            {
+                if (pageViewModels == null) pageViewModels = new List<IPageViewModel>();
+                return pageViewModels;
+            }
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void ChangePageViewModel(IPageViewModel viewModel)
+        {
+            MessageBox.Show("Ok: " + viewModel.Name);
+
+            if (!PageViewModels.Contains(viewModel))
+                PageViewModels.Add(viewModel);
+
+            CurrentPageViewModel = PageViewModels.FirstOrDefault(vm => vm == viewModel);
+        }
+
+        protected override void NotifyProperty([CallerMemberName] string str = "")
+        {
+            base.NotifyProperty(str);
+        }
+
+        #endregion
+
+        public ApplicationViewModel()
+        {
+            PageViewModels.Add(new HomeViewModel());
+            PageViewModels.Add(new ColorViewModel());
+
+            CurrentPageViewModel = PageViewModels[0];
+
+            changePageCommand = new RelayCommand<IPageViewModel>(
+                    p => ChangePageViewModel((IPageViewModel)p),
+                    p => p is IPageViewModel
+            );
         }
     }
 }
