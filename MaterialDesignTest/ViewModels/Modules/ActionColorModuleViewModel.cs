9e9d3f3dbab037ee09358aa55b362ad33518e588
botf <victor.mendele68@gmail.com>
2018-04-03 14:03:08 +0200

    Ajout module action couleur

---
+++ MaterialDesignTest/ViewModels/Modules/ActionColorModuleViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DmxController.ViewModels.Modules
+{
+    public class ActionColorModuleViewModel : IModuleViewModel
+    {
+        private IPageViewModel parent;
+
+        public string ModuleName
+        {
+            get
+            {
+                return "ActionColorModule";
+            }
+        }
+
+        public IPageViewModel Parent
+        {
+            get
+            {
+                return parent;
+            }
+
+            set
+            {
+                parent = value;
+            }
+        }
+    }
+}
