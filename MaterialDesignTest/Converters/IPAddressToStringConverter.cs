c5a7cb4b9f8e8c17fe33f9827077885b299d4d8b
botflux <victor.mendele68@gmail.com>
2018-05-27 16:37:17 +0200

    Ajout correction interface

---
+++ MaterialDesignTest/Converters/IPAddressToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DmxController.Converters
+{
+    public class IPAddressToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((IPAddress)value).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            IPAddress iPAddress;
+            if (IPAddress.TryParse((string) value, out iPAddress))
+            {
+                return iPAddress;
+            }
+            else
+            {
+                return IPAddress.Any;
+            }
+        }
+    }
+}
